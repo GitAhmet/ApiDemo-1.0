229d25bd096ee1a26320071adbc79e6ad3940b82
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2022-11-28 23:54:54 +0100

    Update Program.cs

--- MinimalApiDemo/Program.cs
+++ MinimalApiDemo/Program.cs
@@ -1,7 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -20,6 +18,8 @@ var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
+
+app.MapGet("/testingweatherforecast", () => "Test 1.2.3");
 
 app.MapGet("/weatherforecast", () =>
 {
